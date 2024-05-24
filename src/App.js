e72bb847761e498456bb90af3975bae136e31b10
Julia von Goetz <jvongoetz808@gmail.com>
2024-05-23 18:59:51 -0700

    content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import SineWaveText from './SineWaveText';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <SineWaveText />
+      <div className="content">
+        <h1>Get Early App Access!</h1>
+        <p>
+          Pantry is a collection of recipes that are easy to make with ingredients you already have in your kitchen.
+        </p>
     </div>
+    </div>
+    
   );
 }
 
