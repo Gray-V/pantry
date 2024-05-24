5d47049f0b657ae6e5390b1fe550774ba174e7a8
Julia von Goetz <jvongoetz808@gmail.com>
2024-05-23 23:50:10 -0700

    adding content

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import SineWaveText from './SineWaveText';
 import './App.css';
 import Socials from './Socials';
+import EarlyAccess from './EarlyAccess';
+
 function App() {
   return (
     <div className="App">
       <Socials />
       <SineWaveText />
-      
-
-      <div className="content">
-        <h1>Get Early App Access!</h1>
-        <p>
-          Pantry is a collection of recipes that are easy to make with ingredients you already have in your kitchen.
-        </p>
-    </div>
+      <h1>Get Early App Access!</h1>
+      <EarlyAccess />
     </div>
     
   );
---
+++ src/EarlyAccess.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+const EarlyAccess = () => {
+  return (
+    <div className="content">
+      <div className="info">
+        <p>Join Pantry And Discover Endless Culinary Possibilities!</p>
+        <p>Here's To Rediscovering The Joy Of Cooking And Sharing — Because Food Tastes Better When Shared With Others.</p>
+      </div>
+      <form className="access-form">
+        <div className="form-group-inline">
+          <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" name="firstName" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" name="lastName" required />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone</label>
+          <input type="tel" id="phone" name="phone" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <button type="submit" className="register-button">REGISTER</button>
+      </form>
+    </div>
+  );
+}
+
+export default EarlyAccess;
